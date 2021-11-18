68e8f23eef84c8cc2124b30aa4f1cf668a49a043
t00r0 <47583736+t00r0@users.noreply.github.com>
2021-11-18 16:35:01 -0500

    Create query_domain_users.ps1

---
+++ query_domain_users.ps1
@@ -0,0 +1,25 @@
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain() 
+$PDC = ($domainObj.PdcRoleOwner).Name
+$SearchString = "LDAP://"
+$SearchString += $PDC + "/"
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+$SearchString += $DistinguishedName
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString) 
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+$Searcher.SearchRoot = $objDomain
+
+#Filter the below as needed
+#Default is to search for all user objects: SAM_USER_OBJECT 0x30000000
+#Other filters could include: $Searcher.filter="name=Jeff_Admin"
+
+$Searcher.filter="samAccountType=805306368"
+
+$Result = $Searcher.FindAll()
+Foreach($obj in $Result)
+{
+  Foreach($prop in $obj.Properties)
+  {
+    $prop
+  }
+Write-Host "------------------------" 
+}
